8526828271be6d075d4342384d1561b764402220
Sgenne <simon.genne@gmail.com>
2022-06-05 23:06:58 +0200

    new todo page

--- components/todo/NewTodoPage.tsx
+++ components/todo/NewTodoPage.tsx
@@ -1,16 +1,25 @@
 import styles from "../../styles/todo/NewTodoPage.module.css";
 
-
 const NewTodoPage = () => {
   return (
-    <div className={`d-flex justify-content-center align-items-center ${styles["page"]}`}>
-
-      <div className={styles["new-todo-container"]}>
-        <h1>Dick and balls</h1>
+    <div
+      className={`d-flex justify-content-center align-items-center ${styles["page"]}`}
+    >
+      <div className={styles["new-todo-area"]}>
+        <div className={styles["title-container"]}>
+          <label htmlFor="title-input">Enter a title:</label>
+          <input id="title-input" className={styles["title-input"]} type="text" />
+        </div>
+        <div className={styles["description-input-container"]}>
+          <label htmlFor="description-input">Enter a description:</label>
+          <textarea
+            id="description-input"
+            className={styles["description-input"]}
+          ></textarea>
+        </div>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default NewTodoPage
+export default NewTodoPage;
